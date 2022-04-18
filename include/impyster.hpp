9da3f12675d1d560dc5771eaa561dccaf429d8dd
Magnus Hegdahl <magnus.hegdahl@gmail.com>
2022-04-18 22:25:04 +0200

    move cpt library here

---
+++ include/impyster.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#define _ __unused __attribute__((unused))
+#include <impyster/containers.hpp>
+#include <impyster/enumerate.hpp>
+#include <impyster/map.hpp>
+#include <impyster/print.hpp>
+#include <impyster/range.hpp>
+#include <impyster/zip.hpp>
