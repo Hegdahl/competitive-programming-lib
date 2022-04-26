e398cde156fd691af5a2bd0ed352a8ec3ec974a7
Magnus Hegdahl <magnus.hegdahl@gmail.com>
2022-04-27 00:51:01 +0200

    add default main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#include <impyster.hpp>
+#include <utils.hpp>
+
+using namespace std;
+using namespace impyster;
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(false);
+
+  int T = in;
+  while (T--) {
+  }
+}
